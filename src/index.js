6ed46a8dbbf5d42b699cab29569d4a1d18802068
Thales Avila <thalespizza@gmail.com>
2019-03-12 13:46:00 -0300

    Implementando o Componente CommentDetail

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 // Dependências
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 // Create React Component
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Thales
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">
-            Nice blog post!
-          </div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />      
     </div>
+    
   )
 }
 // Render
