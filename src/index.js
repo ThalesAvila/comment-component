1b817376d8aa07156222b22c8b38da29a58f48aa
Thales Avila <thalespizza@gmail.com>
2019-03-12 15:27:25 -0300

    Configurando CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 // Dependências
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create React Component
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />      
+      <ApprovalCard />
+      <CommentDetail
+        author="Thales"
+        timeAgo="Today at 4:45PM"
+        content="What a post!"
+        img={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="André"
+        timeAgo="Today at 2:00AM"
+        content="Awesome a post!"
+        img={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Geovani"
+        timeAgo="Yesterday at 5:00PM"
+        content="Not a post!"
+        img={faker.image.avatar()}
+      />
     </div>
-    
-  )
-}
+  );
+};
 // Render
 ReactDOM.render(<App />, document.getElementById('root'));
