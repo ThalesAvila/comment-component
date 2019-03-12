5291861bb0db994811c76b8ba9905ac2007b5b5a
Thales Avila <thalespizza@gmail.com>
2019-03-12 13:29:19 -0300

    Adicionando faker como dep

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 // Dependências
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 // Create React Component
 const App = () => {
   return (
-    <div>
-      Hi there!
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Thales
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">
+            Nice blog post!
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
